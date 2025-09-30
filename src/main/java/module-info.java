b2f2b10f143259ffa89c13c03122f2e9109812f5
Ofigel86 <michaofigel@gmail.com>
2025-09-30 15:02:55 +0300

    module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module deobfuscator {
+    requires java.base;
+    requires org.objectweb.asm;
+    requires weka;
+    requires javafx.controls;
+    requires javafx.fxml;
+    exports com.example;
+}
